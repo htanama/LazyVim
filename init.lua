9dfeb7bd6547f27f9a1760544ab90db3072d167d
Harry Tanama <39251813+htanama@users.noreply.github.com>
2025-08-17 16:09:20 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,25 @@
 require("config.lazy")
 
+vim.opt.guicursor = 'n:block,v:block,i:block'
+
+-- Makes the block cursor blink with a 100ms on-time
+vim.opt.guicursor = 'n:block-blinkon100,v:block-blinkon100,i:block-blinkon100'
+vim.opt.guicursor = 'n:block,v:block,i:block-blinkon500'
+
+-- Sets the number of spaces a Tab is worth
+vim.opt.tabstop = 4
+
+-- Sets the number of spaces to indent with automatically
+vim.opt.shiftwidth = 4
+
+-- Makes the <Tab> key insert spaces instead of a Tab character
+vim.opt.expandtab = true
+
+-- Indent automatically when you press <Enter>
+vim.opt.autoindent = true
+
+-- Better automatic indentation for C-like languages
+vim.opt.smartindent = true
+
+-- Use shiftwidth for <Tab> and <Backspace>
+vim.opt.softtabstop = -1
